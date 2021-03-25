91fef2c9909025a44fcb4712d02180ec4ac6ab8b
HemalathaMurugan <hemalatha.psna@gmail.com>
2021-03-25 15:49:43 -0500

    Create restaurant controller and review controller

---
+++ src/main/java/com/cognixia/jump/restaurant/controller/RestaurantController.java
@@ -0,0 +1,75 @@
+package com.cognixia.jump.restaurant.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognixia.jump.restaurant.model.Restaurant;
+import com.cognixia.jump.restaurant.service.RestaurantService;
+
+@RequestMapping("/api")
+@RestController
+public class RestaurantController {
+	
+	@Autowired
+	RestaurantService service;
+	
+	//GET ALL RESTAURANTS
+	@GetMapping("/restaurant")
+	public List<Restaurant> getAllRestaurants() {
+		return service.getAllRestaurants();
+	}
+	
+	//FIND BY ID
+	@GetMapping("/restaurant/id/{id}")
+	public Restaurant getRestaurantById(@PathVariable long id) {
+		return service.getRestaurantById(id);
+	}
+	
+	//FIND BY NAME
+	@GetMapping("/restaurant/name/{name}")
+	public Restaurant getRestaurantByName(@PathVariable String name) {
+		return service.getRestaurantByName(name);
+	}
+	
+	//CREATE RESTAURANT
+	@PostMapping("/restaurant/add")
+	public ResponseEntity<Restaurant> createRestauant(@RequestBody Restaurant res) {
+		
+		Restaurant newRes = service.createRestaurant(res);
+		if(newRes.getRestaurantId()==-1L)
+			return new ResponseEntity<>(newRes, HttpStatus.CONFLICT);
+		return new ResponseEntity<>(newRes, HttpStatus.CREATED);	
+	}
+	
+	//UPDATE RESTAURANT
+	@PutMapping("/restaurant/update")
+	public ResponseEntity<Restaurant> updateRestaurant(@RequestBody Restaurant res) {	
+		Restaurant toBeUpdated = service.updateRestaurant(res);
+		if(toBeUpdated.getRestaurantId()==-1L)
+			return new ResponseEntity<>(toBeUpdated, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(toBeUpdated, HttpStatus.OK);	
+	}
+	
+	//DELETE RESTAURANT
+	@DeleteMapping("/restaurant/delete/{id}")
+	public ResponseEntity<String> deleteRestaurant(@PathVariable long id) {
+		
+		boolean deleted = service.deleteRestaurant(id); 
+		if(!deleted) return new ResponseEntity<>("Restaurant not found", HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<>("Restaurant deleted Successfully", HttpStatus.OK);
+		
+	}
+	
+}
