b9d43fda6616dee12fe4dfe18699fb3e00406e2a
dpvpp <dpvpp22@yahoo.com>
2021-03-25 11:03:55 -0400

    Initial Repos set up

---
+++ src/main/java/com/cognixia/jump/restaurant/repo/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.cognixia.jump.restaurant.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognixia.jump.restaurant.model.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
